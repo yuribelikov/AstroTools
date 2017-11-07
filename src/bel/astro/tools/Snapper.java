4532ff8e2406231c5703d2918aa01ae75d434203
ybelikov <yuribelikov@gmail.com>
2017-11-07 22:55:14 +0300

    3

--- src/bel/astro/tools/Snapper.java
+++ src/bel/astro/tools/Snapper.java
@@ -8,7 +8,7 @@ public class Snapper
 {
   public static void main(String[] args)
   {
-    System.out.println("00000");
+    System.out.println("00000000");
 
     Snapper snapper = new Snapper();
     snapper.click(300, 200);
