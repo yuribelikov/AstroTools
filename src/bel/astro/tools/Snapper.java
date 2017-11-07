d6010b35c101beb2d0ebcaf069358a2f476404bc
ybelikov <yuribelikov@gmail.com>
2017-11-07 22:53:50 +0300

    2

---
+++ src/bel/astro/tools/Snapper.java
@@ -0,0 +1,32 @@
+package bel.astro.tools;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+
+
+public class Snapper
+{
+  public static void main(String[] args)
+  {
+    System.out.println("00000");
+
+    Snapper snapper = new Snapper();
+    snapper.click(300, 200);
+
+  }
+
+  public void click(int x, int y)
+  {
+    try
+    {
+      Robot bot = new Robot();
+      bot.mouseMove(x, y);
+      bot.mousePress(InputEvent.BUTTON1_MASK);
+      bot.mouseRelease(InputEvent.BUTTON1_MASK);
+    }
+    catch (AWTException e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
